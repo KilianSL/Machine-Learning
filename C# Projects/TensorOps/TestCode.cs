982a530e65f52b67fc4d40a10004776210277249
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2019-12-26 15:40:59 +0000

    Started c#proj for working with tensors

---
+++ C# Projects/TensorOps/TestCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TensorOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[,] f = { { 1, 4}, { 7, 1 } };
+            Tensor a = new Tensor(f);
+            a.Display();
+            Console.WriteLine();
+            a.Transpose();
+            a.Display();
+        }
+    }
+}
